2493ae281e0f11f6ce38124ad93fe60197e73220
Koen Romers <info@koenromers.com>
2018-11-16 18:48:22 +0000

    add tidal alias

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -8,3 +8,7 @@ alias week='date +%V'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Tidal
+alias tidal="/Applications/TIDAL.app/Contents/MacOS/./TIDAL"
+alias tidal-stage="/Applications/TIDAL.app/Contents/MacOS/./TIDAL -url=https://desktop-stage.tidal.com"
