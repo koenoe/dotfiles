0768cc657aabe77319bc84f6410d7e7787c5c416
Koen Romers <koenoe@users.noreply.github.com>
2026-01-17 14:06:45 +0000

    Update setup.sh

--- opencode/setup.sh
+++ opencode/setup.sh
@@ -5,3 +5,4 @@ ln -fs ~/dotfiles/opencode/oh-my-opencode.json ~/.config/opencode/oh-my-opencode
 
 npx -y add-skill vercel-labs/agent-skills --skill vercel-react-best-practices --global --yes
 npx -y add-skill expo/skills --skill building-ui api-routes data-fetching dev-client tailwind-setup use-dom --global --yes
+npx -y add-skill callstackincubator/agent-skills --skill react-native-best-practices --global --yes
